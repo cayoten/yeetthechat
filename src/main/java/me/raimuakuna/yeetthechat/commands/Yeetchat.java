afeaea57e0ab3a09097d0fb86150818926f89f3e
cayoten <raimuakuna@foxedin.xyz>
2020-12-15 18:01:21 -0600

    Addition of two permission nodes (yeetchat.bypass and yeetchat.notify)

--- src/main/java/me/raimuakuna/yeetthechat/commands/Yeetchat.java
+++ src/main/java/me/raimuakuna/yeetthechat/commands/Yeetchat.java
@@ -1,10 +1,13 @@
 package me.raimuakuna.yeetthechat.commands;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 
 public class Yeetchat implements CommandExecutor {
     @Override
@@ -15,10 +18,18 @@ public class Yeetchat implements CommandExecutor {
         }
 
         for (Player player: Bukkit.getOnlinePlayers()) {
-            for (int i = 0; i < 101; i++) {
-                player.sendMessage(" ");
+            if (!player.hasPermission("yeetchat.bypass")) {
+                for (int i = 0; i < 101; i++) {
+                    player.sendMessage(" ");
+                }
+
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&6Server&8] &cThe chat has been obliterated by " + sender.getName() + "&c. What was said is no more!\n"));
+
             }
-            player.sendMessage("The chat has been obliterated by " + sender.getName() + ". What was said is no more!");
+
+            if (player.hasPermission("yeetchat.notify")) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&aThe chat was cleared by " + sender.getName() + "."));
+            }
         }
 
         return true;
